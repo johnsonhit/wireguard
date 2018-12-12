2d7923361f6611d9b4a2725571202df2c89d01d5
mengniu8 <34342026+mengniu8@users.noreply.github.com>
2018-12-13 01:30:59 +0800

    Update wireguard_install.sh

--- wireguard_install.sh
+++ wireguard_install.sh
@@ -74,6 +74,7 @@ EOF
 #centos7安装wireguard
 wireguard_install(){
     curl -Lo /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
+    yum install -y epel-release
     yum install -y dkms gcc-c++ gcc-gfortran glibc-headers glibc-devel libquadmath-devel libtool systemtap systemtap-devel
     yum -y install wireguard-dkms wireguard-tools
     yum -y install qrencode
